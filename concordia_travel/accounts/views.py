49987670f82576f52e981010d2fe59504e2eaf63
sepehr7ghfz <spentax9@gmail.com>
2024-02-23 18:38:11 -0500

    login bug fixed

--- concordia_travel/accounts/views.py
+++ concordia_travel/accounts/views.py
@@ -40,15 +40,18 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
+        print('Login method')
         username = request.POST['username']
         password = request.POST['password']
 
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
+            print('User is not none')
             auth.login(request, user)
             return redirect('/')
         else:
+            print('User is none')
             messages.info(request, 'Invalid credentials')
             return redirect('login')
     else:
