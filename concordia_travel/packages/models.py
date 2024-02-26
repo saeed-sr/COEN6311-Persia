2cda543e3c1f408a7f60a80220f504e353e8ae89
sepehr7ghfz <spentax9@gmail.com>
2024-02-26 11:07:45 -0500

    booking view updated

--- concordia_travel/packages/models.py
+++ concordia_travel/packages/models.py
@@ -49,3 +49,12 @@ class CustomPackage(models.Model):
     hotels = models.ManyToManyField(Hotel, blank=True)
     activities = models.ManyToManyField(Activity, blank=True)
     is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        user_info = f"{self.user.username}'s Custom Package:"
+
+        flights_info = "Flights: " + ", ".join(str(flight) for flight in self.flights.all()) if self.flights.exists() else "No flights"
+        hotels_info = "Hotels: " + ", ".join(str(hotel) for hotel in self.hotels.all()) if self.hotels.exists() else "No hotels"
+        activities_info = "Activities: " + ", ".join(str(activity) for activity in self.activities.all()) if self.activities.exists() else "No activities"
+
+        return f"{user_info}\n{flights_info}\n{hotels_info}\n{activities_info}"
