398b701676033ceca3359b0ec66d948512a29db3
sepehr_ghfz <m_falsa@live.concordia.ca>
2024-03-21 03:22:50 -0400

    reviewed previous codes and fixed booking bug

--- concordia_travel/packages/urls.py
+++ concordia_travel/packages/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
 
     path('hotels/', views.HotelListView.as_view(), name='hotel-list'),
     path('hotels/<int:pk>/', views.hotel_detail, name='hotel_detail'),
-    path('hotels/book/<int:pk>/', views.book_flight, name='book_hotel'),
+    path('hotels/book/<int:pk>/', views.book_hotel, name='book_hotel'),
 
     path('activities/', views.ActivityListView.as_view(), name='activity-list'),
     path('activities/<int:pk>/', views.activity_detail, name='activity_detail'),
-    path('activities/book/<int:pk>/', views.book_flight, name='book_activity'),
+    path('activities/book/<int:pk>/', views.book_activity, name='book_activity'),
 
     path('custom-packages/create/', views.create_custom_package, name='create_custom_package'),
     path('custom-packages/<int:pk>/', views.CustomPackageDetailView.as_view(), name='custom-package-detail'),
